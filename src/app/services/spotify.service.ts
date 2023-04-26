ff852312644194075172f8886c03d570533d56a0
Jesus <gabrielj82@gmail.com>
2023-04-25 22:38:52 -0300

    service spotify search

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -6,8 +6,10 @@ import { Injectable } from '@angular/core';
 })
 export class SpotifyService {
 token :string
+url:string
   constructor(private http:HttpClient) {
     console.log("Spotify service ON")
+    this.url='https://api.spotify.com/'
     this.token='BQC-kC2aroYdRxl5E1w_dK1nMYP809PHrhGXc2rNQqSrFjTzCth3_ejz-0WkmxOs2CJafLeC_l6MrGy5zwTiCbn9nSD6TVkanxnWWdgI7x4-E1DTfLQa'
    }
 
@@ -16,14 +18,15 @@ token :string
       Authorization:`Bearer ${this.token}` 
      }) 
      const endpoint='v1/browse/new-releases?country=SE&locale=es-419%2Ces%3Bq%3D0.8&offset=0&limit=20'; 
-    return this.http.get(`https://api.spotify.com/${endpoint}`, { headers });
+    return this.http.get(`${this.url}${endpoint}`, { headers });
           
    }
    getArtist(termino:string){
     const headers = new HttpHeaders({
       Authorization:`Bearer ${this.token}` 
      }) 
-    return this.http.get(`https://api.spotify.com/v1/search?query=${termino}&type=artist&locale=es-419%2Ces%3Bq%3D0.7&offset=0&limit=20`, { headers });
+     const endpoint= `v1/search?query=${termino}&type=artist&locale=es-419%2Ces%3Bq%3D0.7&offset=0&limit=10`
+    return this.http.get(`${this.url}${endpoint}`, { headers });
     
    }
 }
